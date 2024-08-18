e5b75097f99a699fdc1c21b05aed1c3f1d0ebdc8
Rashid <rpg717113@gmail.com>
2024-08-18 21:52:12 +0300

    All functionality is implemented and json is generated

--- src/json_fetch.cpp
+++ src/json_fetch.cpp
@@ -4,6 +4,7 @@
 #include <nlohmann/json.hpp>
 
 #include <unordered_set>
+#include <unordered_map>
 #include <fstream>
 
 
@@ -67,22 +68,33 @@ int main() {
         }
 	std::cout << "All JSON data parsed\n";
 
+	json data;
+
 	// first branch (fb) & second branch (sb)  packages unique names 
 	std::unordered_set<std::string> fb_unique_names, sb_unique_names;
-	
+
+	// maps for output packages to json
+	std::unordered_map<std::string, size_t> fb_packages, sb_packages;
+
+	int fb_packages_count = 0;	
 	for (const auto& package : first_branch_data["packages"]) {
 		
 		fb_unique_names.insert(package.value("name", ""));
-
+		fb_packages[package.value("name", "")] = fb_packages_count++;
+		
 	}
+
+	int sb_packages_count = 0;
 	for (const auto& package : second_branch_data["packages"]) {
 
 		sb_unique_names.insert(package.value("name", ""));
-
+		sb_packages[package.value("name", "")] =  sb_packages_count++;
 	}
-	std::ofstream outfile_1, outfile_2;
+
+	std::ofstream outfile_1, outfile_2, outfile_3;
 	outfile_1.open("fb_names.txt", std::ios::out);
 	outfile_2.open("sb_names.txt", std::ios::out);
+	outfile_3.open("data.txt", std::ios::out);
   	if (!outfile_1) {
 		std::cerr << "Error: unable to create or open file!" << std::endl;
 	}
@@ -91,19 +103,51 @@ int main() {
 		std::cerr << "Error: unable to create or open file!" << std::endl;
 	}
 
-	std::unordered_set<std::string> fbun_copy = fb_unique_names;	
-
+	if (!outfile_3) {
+		std::cerr << "Error: unable to create or open file!" << std::endl;
+	}
+	
+	// copy of first branch packages to find unique packages
+	// in second branch
+	std::unordered_set<std::string> fbun_copy = fb_unique_names, common;	
+        
 	// find unique packages' names 
 
 	//find packages that's in the first branch but not in the second
+	
 	for (const auto& name : sb_unique_names) {
-		fb_unique_names.erase(name);
+		const auto& data_to_insert = first_branch_data["packages"][fb_packages[name]];
+		if (!fb_unique_names.erase(name)) {
+			data["first_branch_packages"].push_back(data_to_insert); 
+ 
+		} else {
+			common.insert(name);
+		}
 	}	
 	
 	// find packages that's in the second branch but not in the first
 	for (const auto& name : fbun_copy) {
-		sb_unique_names.erase(name);
+		const auto& data_to_insert = second_branch_data["packages"][sb_packages[name]];
+		if (!sb_unique_names.erase(name)) {
+			data["second_branch_packages"].push_back(data_to_insert);
+		} else {
+			common.insert(name);
+		}
 	}
+
+	for (const auto& name : common) {
+		auto fb_packages_version = first_branch_data["packages"][fb_packages[name]].value("version", "");	
+		auto sb_packages_version = second_branch_data["packages"][sb_packages[name]].value("version", "");
+		if (fb_packages_version > sb_packages_version) {
+			data["higher_version_release"].push_back(first_branch_data["packages"][fb_packages[name]]);
+			//std::cout << fb_packages_version << " ";
+		}
+		
+	
+
+	}
+
+	std::cout << "\n";
 	
 
 	// output unique names from first branch
@@ -118,6 +162,8 @@ int main() {
 	}
 	outfile_2 << "\n";
 
+	outfile_3 << data.dump(4);
+
 	outfile_1.close();
 	outfile_2.close();	
         
@@ -129,4 +175,5 @@ int main() {
 
     return 0;
 }
+
 
