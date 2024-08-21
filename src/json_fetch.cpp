b310a450af72bc80559fe8718985f8c1c56c2cc6
rvsh10 <rpg717113@gmail.com>
2024-08-21 08:19:18 +0300

    Fixed search for packages

--- src/json_fetch.cpp
+++ src/json_fetch.cpp
@@ -17,7 +17,7 @@ json get_data(const std::string& first_branch, const std::string& second_branch)
     
 	
     // json with all data
-    json data;
+    json data = {};
 	
     CURL* curl;
     CURLcode res;
@@ -29,7 +29,6 @@ json get_data(const std::string& first_branch, const std::string& second_branch)
 
 	std::string api = "https://rdb.altlinux.org/api/export/branch_binary_packages/";
 
-	std::string first_branch = "p10", second_branch = "p9";
 
         // set callback function for data write
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
@@ -76,66 +75,111 @@ json get_data(const std::string& first_branch, const std::string& second_branch)
 	// first branch (fb) & second branch (sb)  packages unique names 
 	std::unordered_set<std::string> fb_unique_names, sb_unique_names;
 
+	std::unordered_set<std::string> fb_archs, sb_archs;
+
 	// maps for output packages to json
 	std::unordered_map<std::string, size_t> fb_packages, sb_packages;
 	
-	int fb_packages_count = 0;
+
+	
+	// NEW CODE
+	
+	std::unordered_map<std::string, std::unordered_map<std::string, size_t>> first_branch_packages;
+	std::unordered_map<std::string, std::unordered_map<std::string, size_t>> second_branch_packages;
+	std::unordered_map<std::string, std::unordered_set<std::string>> common_packages;
+	
+	int fb_data_counter = 0;
 	for (const auto& package : first_branch_data["packages"]) {
 		
-		fb_unique_names.insert(package.value("name", ""));
-		fb_packages[package.value("name", "")] = fb_packages_count++;
-		
+		const auto& arch = package.value("arch", "xxx");
+		const auto& package_name = package.value("name", "");
+
+		first_branch_packages[arch][package_name] = fb_data_counter++;
 	}
 
-	int sb_packages_count = 0;
+	int sb_data_counter = 0;
 	for (const auto& package : second_branch_data["packages"]) {
+		
+		const auto& arch = package.value("arch", "");
+		const auto& package_name = package.value("name", "");
 
-		sb_unique_names.insert(package.value("name", ""));
-		sb_packages[package.value("name", "")] =  sb_packages_count++;
+		second_branch_packages[arch][package_name] = sb_data_counter++;
+	}
+		
+	
+	// find unique packages in first branch
+	// and insert higher release version
+	for (const auto& archs_pnames_idxs : second_branch_packages) {
+		const auto& arch = archs_pnames_idxs.first;
+		const auto& pnames_idxs = archs_pnames_idxs.second; // pair
+		
+		json arch_packs = {};
+		for (const auto& names_idxs : pnames_idxs) {
+			const auto& name = names_idxs.first;
+			
+			if (first_branch_packages[arch].find(name) != 
+					first_branch_packages[arch].end()) {
+				const auto& fp_idx = first_branch_packages[arch][name];	
+				const auto& sp_idx = second_branch_packages[arch][name];	
+				const auto& fb_package_version = first_branch_data["packages"][fp_idx].value("version", "");
+				const auto& sb_package_version = second_branch_data["packages"][sp_idx].value("version", "");;
+				if (fb_package_version > sb_package_version) {
+					arch_packs[arch].push_back(first_branch_data["packages"][fp_idx]);
+				}
+				
+				common_packages[arch].insert(name);;
+				first_branch_packages[arch].erase(name);
+			}
+
+		}
+
+		data["higher_version_release"].push_back(arch_packs);
 	}
 
 	
-	// copy of first branch packages to find unique packages
-	// in second branch
-	std::unordered_set<std::string> fbun_copy = fb_unique_names, common;	
-        
-	// find unique packages' names 
-
-	//find packages that's in the first branch but not in the second
-	
-	for (const auto& name : sb_unique_names) {
-		const auto& data_to_insert = first_branch_data["packages"][fb_packages[name]];
-		if (!fb_unique_names.erase(name)) {
-			data["first_branch_packages"].push_back(data_to_insert); 
- 
-		} else {
-			common.insert(name);
-		}
-	}	
-	
-	// find packages that's in the second branch but not in the first
-	for (const auto& name : fbun_copy) {
-		const auto& data_to_insert = second_branch_data["packages"][sb_packages[name]];
-		if (!sb_unique_names.erase(name)) {
-			data["second_branch_packages"].push_back(data_to_insert);
-		} else {
-			common.insert(name);
+	// second branch unique packages
+	for (const auto& archs_pnames_idxs : common_packages) {
+		const auto& arch = archs_pnames_idxs.first;
+		const auto& pnames = archs_pnames_idxs.second; // pair
+		
+		for (const auto& name: pnames) {
+			second_branch_packages[arch].erase(name);
 		}
 	}
-
-	// find packages from first branch that have higher version release than second 
-	for (const auto& name : common) {
-		auto fb_packages_version = first_branch_data["packages"][fb_packages[name]].value("version", "");	
-		auto sb_packages_version = second_branch_data["packages"][sb_packages[name]].value("version", "");
-		if (fb_packages_version > sb_packages_version) {
-			data["higher_version_release"].push_back(first_branch_data["packages"][fb_packages[name]]);
 	
+
+	
+	// insert unique second branch packages	
+	for (const auto& archs_pnames_idxs : second_branch_packages) {
+		const auto& arch = archs_pnames_idxs.first;
+		const auto& pnames_idxs = archs_pnames_idxs.second; // pair
+
+		json arch_packs = {};
+		for (const auto& names_idxs : pnames_idxs) {
+			const auto& idx = names_idxs.second;
+			arch_packs[arch].push_back(second_branch_data["packages"][idx]);
 		}
+		data["second_branch_unique"].push_back(arch_packs);
+
 	}
 
-	// freeing up resources
+	
+
+	// insert unique first branch packages	
+	for (const auto& archs_pnames_idxs : first_branch_packages) {
+		const auto& arch = archs_pnames_idxs.first;
+		const auto& pnames_idxs = archs_pnames_idxs.second; // pair
+
+		json arch_packs = {};
+		for (const auto& names_idxs : pnames_idxs) {
+			const auto& idx = names_idxs.second;
+			arch_packs[arch].push_back(first_branch_data["packages"][idx]);
+		}
+		data["first_branch_unique"].push_back(arch_packs);
+	}
+
         curl_easy_cleanup(curl);
-    }
+    } 
     curl_global_cleanup();
 
     return data;
