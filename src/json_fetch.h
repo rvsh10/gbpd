5f544e37fceaf878e481c5576f0f4a4f9dbb3f51
rvsh10 <rpg717113@gmail.com>
2024-08-22 12:02:11 +0300

    Updated header file

--- src/json_fetch.h
+++ src/json_fetch.h
@@ -1,6 +1,7 @@
 #ifndef JSON_FETCH_LIB
 #define JSON_FETCH_LIB
 
+#include <nlohmann/json_fwd.hpp>
 #include <string>
 #include <nlohmann/json.hpp>
 
@@ -8,5 +9,7 @@
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
 
 nlohmann::json get_data(const std::string& first_branch, const std::string& second_branch);
+
+nlohmann::json fetch_json(std::string branch); 
 
 #endif
